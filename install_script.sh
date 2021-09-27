29047ba95599c171e9c6c013e7404a5cac59dd06
Andley S <39422775+Andley302@users.noreply.github.com>
2021-09-27 17:24:06 -0300

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #INSTALA PACOTES
-echo -ne "\033[1;32m INSTALADOR CHECKUSER!INICIANDO...\033[1;37m ";
+echo -ne "\033[1;32m INSTALADOR CheckUser!INICIANDO...\033[1;37m ";
 sleep 5;
 apt-get update && apt-get install git crontab -y;
 git clone https://github.com/Andley302/CheckUser.git;
+cd /root/CheckUser;
 clear;
-mkdir /root/checkuser;
-cd /root/checkuser;
 chmod +x iptables.sh;
 ./iptables.sh;
 chmod +x startcheck.sh;
@@ -22,13 +21,13 @@ npm install;
 #SETCONFIG FUNCAO
 set_config () {
 echo -ne "\033[1;32m INFORME A SENHA DO ADMIN DO PAINEL(PHPMYADMIN/MYSQL)\033[1;37m: "; read name
-sed -i "s;1234;$name;g" /root/checkuser/.env > /dev/null 2>&1
+sed -i "s;1234;$name;g" /root/CheckUser/.env > /dev/null 2>&1
 }
 
 #END FUNCAO
 finalizar () {
 #CRONTAB
-echo '* * * * * root cd /root/checkuser && ./startcheck.sh' >> /etc/crontab
+echo '* * * * * root cd /root/CheckUser && ./startcheck.sh' >> /etc/crontab
 service cron reload;
 service cron restart;
 
@@ -37,7 +36,7 @@ service cron restart;
 clear;
 echo -ne "\033[1;32m FINALIZADO!\033[1;37m ";
 echo "";
-echo "Se deseja alterar a porta ou senha,basta editar o arquivo /root/checkuser/.env";
+echo "Se deseja alterar a porta ou senha,basta editar o arquivo /root/CheckUser/.env";
 }
 
 #EXECUTA FUNCAO
